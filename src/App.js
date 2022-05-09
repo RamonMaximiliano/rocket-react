c2619d5987d5b6bc12b4f201f22aa23b9803a732
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-05-08 22:57:32 -0300

    fixed delete function

--- src/App.js
+++ src/App.js
@@ -13,89 +13,78 @@ function App() {
   // "useState" é um hook (useEffect é outro exemplo), geralmente começam com "use" que são funções que permitem ligar e conectar os recursos de estado dos componentes
   // Hooks foram criados para não mais usar classes no desenv, e sim usar functions 
 
-function handleItem() {
+  function handleItem() {
 
-  //pegando a lista de items, caso não exista nenhum item, assignando o index como 0, caso existam dai ele pega o id do ultimo item da lista e adiciona 1, assim nunca haverá 2 items com o mesmo ID.
+    //pegando a lista de items, caso não exista nenhum item, assignando o index como 0, caso existam dai ele pega o id do ultimo item da lista e adiciona 1, assim nunca haverá 2 items com o mesmo ID.
 
 
-  let itemsList2 = document.querySelectorAll("small")
-  let itemIndex = Array.from(itemsList2)
-  let itemIndexLength = 0
+    let itemsList2 = document.querySelectorAll("small")
+    let itemIndex = Array.from(itemsList2)
+    let itemIndexLength = 0
 
-if (itemIndex.length <= 0 ){
-  itemIndexLength = 0
-} else {
-  itemIndexLength = itemIndex[itemIndex.length-1].innerText
-}
-let endId = parseInt(itemIndexLength) +1 
+    if (itemIndex.length <= 0) {
+      itemIndexLength = 0
+    } else {
+      itemIndexLength = itemIndex[itemIndex.length - 1].innerText
+    }
+    let endId = parseInt(itemIndexLength) + 1
 
     let newItem = ''
-    if(itemName == ''){
+    if (itemName == '') {
       window.alert("Type something!")
     } else {
-    newItem = {
-      name: itemName,
-      id: endId
+      newItem = {
+        name: itemName,
+        id: endId
+      }
+
+      setItems(prevState => [...prevState, newItem]);
+      document.getElementById("textInput").value = ''
     }
 
-    setItems(prevState => [...prevState, newItem]);
-    document.getElementById("textInput").value = ''
-  }
 
-
-/*   let itemsList2 = document.querySelectorAll("small")
-  // Used the Array.from() method to convert a nodelist to an array  
-  let itemIndex = Array.from(itemsList2)
-  let itemIndexLength = itemIndex[itemIndex.length-1].innerText
-  console.log(itemIndexLength) */
+    /*   let itemsList2 = document.querySelectorAll("small")
+      // Used the Array.from() method to convert a nodelist to an array  
+      let itemIndex = Array.from(itemsList2)
+      let itemIndexLength = itemIndex[itemIndex.length-1].innerText
+      console.log(itemIndexLength) */
 
 
     //was not able to understand the below property being sent, check arrow functions and try to make normal function on the below
   };
 
-//Test sfe
+  //Test sfe
 
-  function clearList(){
+  function clearList() {
     setItems([])
   }
 
 
+  const testItem = function deleteItem(id) {
+    console.log(id)
+
+    // Eu estava tentando deletar os items da lista com o AddEvent Listener, porém em react como cada item da lista é um component, fica muito dificil pois o componente é montado renderizado e desmontado, conforme link abaixo:
+
+    /*   https://www.pluralsight.com/guides/event-listeners-in-react-components
+     */
 
 
-
-const testItem = function deleteItem(){
-
-/*   https://www.pluralsight.com/guides/event-listeners-in-react-components
- */
-  let itemsList = document.querySelectorAll("small")
+    //Usar o parametro de ID abaixo conforme a react to do do Traversy media é mais facil de fazer, passa o id do component como parametro na function, e ela fica disponivel na função delete para usar como identifier para atualizar a lista.
 
 
-  console.log(itemsList)
-
-
-  for(let other of itemsList){
-    other.addEventListener("click", function showItem(event){
-      console.log(`olha ${event.target.innerText}`)
-       let deletedList = items.filter(function(item){
-        return item.id != event.target.innerText
-      })
-      setItems(deletedList) 
+    let deletedList = items.filter(function (item) {
+      return item.id != id
     })
-  }
 
-
-
-}
-
-
-
+    setItems(deletedList)
+  };
 
   //USE-EFFECT
   // "useEffect" ele é renderizado assim que a tela é carregada.
   // se o array dele é deixado vazio então ele é executado somente uma vez, se o array tiver states, então o useEffect é acionado cada vez que o state muda
 
   useEffect(() => {
-       fetch("https://api.github.com/users/ramonmaximiliano")
+    fetch("https://api.github.com/users/ramonmaximiliano")
       .then(function (result) {
         return result.json()
       })
@@ -122,12 +111,12 @@ const testItem = function deleteItem(){
       <input id="textInput" type="text" placeholder="Type your item" onChange={e => setItemName(e.target.value)}></input>
       <button onClick={handleItem}>Add</button>
       {
-        items.map(item => <Card name={item.name} id={item.id} onclick={testItem}/>)
+        items.map(item => <Card name={item.name} id={item.id} onclick={testItem} />)
 
       }
       <div>
-      <button id="clearButton" onClick={clearList}>Clear</button>
-      <footer className="footer">Created by Ramon</footer>
+        <button id="clearButton" onClick={clearList}>Clear</button>
+        <footer className="footer">Created by Ramon</footer>
       </div>
 
     </div>
@@ -135,11 +124,12 @@ const testItem = function deleteItem(){
 }
 export default App;
 
-// Adicionar button para excluir o item
+// --Adicionar button para excluir o item
 // -- Não deixar adicionar itens vazios
 // -- Adicionar button de esvaziar lista
 // -- Adicionar footer criado por Ramon
 // -- Quando adicionar item, esvaziar o input box
+// Limpar o value da input para que quando clieque add de novo ele não fique adicionando o mesmo item várias vezes
 // Ajustar cores e interface
 // Re-assistir video Diego (React)
 // Fazer o deploy com link
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,9 @@ function Card(props) {
     return (
         <div className="card" id={props.id} >
             <strong>{props.name}</strong>
-            <small onClick={props.onclick}>{props.id}</small>
+
+            {/*             passando o id do component como parametro para a function delete para poder atualizar a lista de items, mais facil assim do que com o event listener do vanila javascript
+ */}            <small onClick={() => props.onclick(props.id)}>{props.id}</small>
         </div>
     );
 }
